99df50958e6db6e6259e63896d263afc745e18de
Vinita Mathew <vinitamathew2010@gmail.com>
2023-03-01 12:55:19 +0530

    created standards component

---
+++ src/components/HomePage/standards/Standards.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import "./Standards.scss";
+import StandardsData from "./standardsData.js";
+
+export default function Standards() {
+    const [ref, inView] = useInView({
+      threshold: 0.3, // Trigger when the component is 50% visible
+      triggerOnce: true, // Only trigger once
+    });
+  return (
+    <div
+      ref={ref}
+      className={
+        inView ? "slide-up standardsContainer" : "hidden standardsContainer"
+      }
+    >
+      <h2 className="standards-heading">Best in the world standards</h2>
+      <div className="standards-description">
+        Something about how important it is to have these standards would come
+        here
+      </div>
+      <table className="standards-table">
+        <tr className="standards-table-row row1">
+          <td className="standards-table-data">
+            <img className="logo logo1" src={StandardsData[0].logo} />
+            <div className="description-wrapper">
+              <div className="description">{StandardsData[0].description}</div>
+            </div>
+          </td>
+          <td className="standards-table-data">
+            <img className="logo logo2" src={StandardsData[1].logo} />
+            <div className="description-wrapper">
+              <div className="description">{StandardsData[1].description}</div>
+            </div>
+          </td>
+        </tr>
+        <tr className="standards-table-row row2">
+          <td className="standards-table-data">
+            <img className="logo logo3" src={StandardsData[2].logo} />
+            <div className="description-wrapper">
+              <div className="description">{StandardsData[2].description}</div>
+            </div>
+          </td>
+          <td className="standards-table-data">
+            <img className="logo logo4" src={StandardsData[3].logo} />
+            <div className="description-wrapper">
+              <div className="description">{StandardsData[3].description}</div>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
