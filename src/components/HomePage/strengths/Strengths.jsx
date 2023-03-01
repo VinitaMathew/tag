63e07388e5c752d3c968dce498e4d7f279181a8b
Vinita Mathew <vinitamathew2010@gmail.com>
2023-03-01 12:54:35 +0530

    changed carousel library

--- src/components/HomePage/strengths/Strengths.jsx
+++ src/components/HomePage/strengths/Strengths.jsx
@@ -1,163 +1,89 @@
 import React from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
+import {
+  StackedCarousel,
+  ResponsiveContainer,
+} from "react-stacked-center-carousel";
 import "./Strengths.scss";
+import StrengthsData from "./strendthsData.js";
 
-// import required modules
-import { EffectCoverflow, Pagination, Navigation} from "swiper";
-
-export default function Strengths() {
+export default function ResponsiveCarousel(props) {
+  const ref = React.useRef();
   return (
-    <section>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={"2"}
-        navigation={true}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 500,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
+    <div style={{ width: "100%", position: "relative" }}>
+      <ResponsiveContainer
+        carouselRef={ref}
+        render={(parentWidth, carouselRef) => {
+          // If you want to use a ref to call the method of StackedCarousel, you cannot set the ref directly on the carousel component
+          // This is because ResponsiveContainer will not render the carousel before its parent's width is determined
+          // parentWidth is determined after your parent component mounts. Thus if you set the ref directly it will not work since the carousel is not rendered
+          // Thus you need to pass your ref object to the ResponsiveContainer as the carouselRef prop and in your render function you will receive this ref object
+          let currentVisibleSlide = 5;
+          if (parentWidth <= 1100) currentVisibleSlide = 3;
+          if (parentWidth <= 1080) currentVisibleSlide = 1;
+          return (
+            <StackedCarousel
+              ref={carouselRef}
+              slideComponent={Card}
+              slideWidth={parentWidth < 800 ? parentWidth - 140 : 650}
+              carouselWidth={parentWidth}
+              data={StrengthsData}
+              currentVisibleSlide={currentVisibleSlide}
+              maxVisibleSlide={5}
+              useGrabCursor
+              fadeDistance={0.7}
+              swipeThreshold={3}
+            />
+          );
         }}
-        spaceBetween={-300}
-        loop={true}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide className="swiper-slide--one">
-          <div>
-            <h2>Jellyfish</h2>
-            <p>
-              Jellyfish and sea jellies are the informal common names given to
-              the medusa-phase of certain gelatinous members of the subphylum
-              Medusozoa, a major part of the phylum Cnidaria.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Jellyfish" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide--two">
-          <div>
-            <h2>Seahorse</h2>
-            <p>
-              Seahorses are mainly found in shallow tropical and temperate salt
-              water throughout the world. They live in sheltered areas such as
-              seagrass beds, estuaries, coral reefs, and mangroves. Four species
-              are found in Pacific waters from North America to South America.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Seahorse" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide--three">
-          <div>
-            <h2>octopus</h2>
-            <p>
-              Octopuses inhabit various regions of the ocean, including coral
-              reefs, pelagic waters, and the seabed; some live in the intertidal
-              zone and others at abyssal depths. Most species grow quickly,
-              mature early, and are short-lived.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Octopus" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide--four">
-          <div>
-            <h2>Jellyfish</h2>
-            <p>
-              Jellyfish and sea jellies are the informal common names given to
-              the medusa-phase of certain gelatinous members of the subphylum
-              Medusozoa, a major part of the phylum Cnidaria.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Jellyfish" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide--one">
-          <div>
-            <h2>Dolphin</h2>
-            <p>
-              Dolphins are widespread. Most species prefer the warm waters of
-              the tropic zones, but some, such as the right whale dolphin,
-              prefer colder climates. Dolphins feed largely on fish and squid,
-              but a few, such as the orca, feed on large mammals such as seals.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Dolphin" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide--five">
-          <div>
-            <h2>Shark</h2>
-            <p>
-              Sharks are a group of elasmobranch fish characterized by a
-              cartilaginous skeleton, five to seven gill slits on the sides of
-              the head, and pectoral fins that are not fused to the head.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Shark" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide--two">
-          <div>
-            <h2>Dolphin</h2>
-            <p>
-              Dolphins are widespread. Most species prefer the warm waters of
-              the tropic zones, but some, such as the right whale dolphin,
-              prefer colder climates. Dolphins feed largely on fish and squid,
-              but a few, such as the orca, feed on large mammals such as seals.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Dolphin" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide--three">
-          <div>
-            <h2>Shark</h2>
-            <p>
-              Sharks are a group of elasmobranch fish characterized by a
-              cartilaginous skeleton, five to seven gill slits on the sides of
-              the head, and pectoral fins that are not fused to the head.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Shark" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide--five">
-          <div>
-            <h2>octopus</h2>
-            <p>
-              Octopuses inhabit various regions of the ocean, including coral
-              reefs, pelagic waters, and the seabed; some live in the intertidal
-              zone and others at abyssal depths. Most species grow quickly,
-              mature early, and are short-lived.
-            </p>
-            <a href="https://en.wikipedia.org/wiki/Octopus" target="_blank">
-              explore
-            </a>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </section>
+      />
+      <>
+        <div
+          style={{ position: "absolute", top: "110%", left: "45%", zIndex: 10 }}
+          onClick={() => {
+            ref.current?.goBack();
+          }}
+        >
+          {"<"}
+        </div>
+        <div
+          style={{ position: "absolute", top: "110%", right: "45%", zIndex: 10 }}
+          onClick={() => {
+            ref.current?.goNext(6);
+          }}
+        >
+          {">"}
+        </div>
+      </>
+    </div>
   );
 }
+
+// Very import to memoize your Slide component otherwise there might be performance issue
+// At minimum your should do a simple React.memo(SlideComponent)
+// If you want the absolute best performance then pass in a custom comparator function like below
+export const Card = React.memo(function (props) {
+  const { data, dataIndex } = props;
+  const { cover ,title} = data[dataIndex];
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: 400,
+        userSelect: "none",
+      }}
+      className="my-slide-component"
+    >
+      <img
+        style={{
+          height: "100%",
+          width: "100%",
+          objectFit: "cover",
+          borderRadius: 20,
+        }}
+        draggable={false}
+        src={cover}
+      />
+      <div className="slide-title">{title}</div>
+    </div>
+  );
+});
