7c3ec0b0377f813cddd6d3bb50a116bc70e0d674
Vinita Mathew <vinitamathew2010@gmail.com>
2023-03-06 16:52:27 +0530

    routing changes

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -1,5 +1,5 @@
-import React,{useRef,useState} from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useRef } from "react";
+import { BrowserRouter as Route, Routes, HashRouter } from "react-router-dom";
 import Home from "../HomePage/Home";
 import Menu from "../menu/Menu";
 import AboutPage from "../AboutPage/AboutPage";
@@ -16,32 +16,32 @@ const Careers = () => (
 );
 
 export default function Routing() {
- const ref = useRef(null);
- const scrollToHome = () => {
-   window.scrollTo({
-     top: 0,
-     behavior: "smooth",
-   });
- };
-  const scrollToContact=() =>{
+  const ref = useRef(null);
+  const scrollToHome = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  const scrollToContact = () => {
     ref.current.scrollIntoView({ behavior: "smooth" });
-  }
-  
-    return (
-      <div className="routingContainer">
-        <Router>
-          <div className="tag-menu-container">
-            <Menu contactClick={scrollToContact} />
-          </div>
-          <Routes>
-            <Route path="/" element={<Home contactClick={scrollToContact} />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/careers" element={<Careers />} />
-          </Routes>
-          <Contact ref={ref} homeClick={scrollToHome} />
-        </Router>
-      </div>
-    );
-  }
+  };
+
+  return (
+    <div className="routingContainer">
+      <HashRouter>
+        <div className="tag-menu-container">
+          <Menu contactClick={scrollToContact} />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home contactClick={scrollToContact} />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/careers" element={<Careers />} />
+        </Routes>
+        <Contact ref={ref} homeClick={scrollToHome} />
+      </HashRouter>
+    </div>
+  );
+}
